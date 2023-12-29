e4b54eb0b82f8262cea9954e9737027ce614ceac
sien <tornike.beridze264@gmail.com>
2023-12-29 23:56:44 +0400

    final project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect, useState }  from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
+import BlogPage from './components/blogPage/BlogPage';
+import AddBlog from './components/addBlog/AddBlog';
 
 function App() {
+
+  const [loginOrAdd, setLoginOrAdd] = useState("შესვლა")
+
+  useEffect(() => {
+    localStorage.setItem('token', '85e88f7b06be2f5eba5dcef4a8a067e148d81f655499affd50a1e42e49d1aaa1')
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home loginOrAdd={loginOrAdd} setLoginOrAdd={setLoginOrAdd}/>} />
+        <Route path='/blog/:id' element={<BlogPage loginOrAdd={loginOrAdd} setLoginOrAdd={setLoginOrAdd}/>} />
+        <Route path='blog/add' element={<AddBlog loginOrAdd={loginOrAdd}/>} />
+      </Routes>
     </div>
   );
 }
