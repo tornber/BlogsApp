3f36730e977c218af93852a5af3ca9ddea15590a
sien <tornike.beridze264@gmail.com>
2023-12-30 15:16:55 +0400

    Updates

---
+++ precache-manifest.d76f3f88fc46f53d2e94456eb0cd1af1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1021c23296d9ed1da0e4134cb8876c",
+    "url": "/BlogsApp/index.html"
+  },
+  {
+    "revision": "f6c3737f8a1e860fbb27",
+    "url": "/BlogsApp/static/css/main.620ddc9e.chunk.css"
+  },
+  {
+    "revision": "d8b4e97b3e4b7888a38b",
+    "url": "/BlogsApp/static/js/2.57007c8e.chunk.js"
+  },
+  {
+    "revision": "60c46b200b0cfd41617f",
+    "url": "/BlogsApp/static/js/3.b265e9c7.chunk.js"
+  },
+  {
+    "revision": "f6c3737f8a1e860fbb27",
+    "url": "/BlogsApp/static/js/main.361601e1.chunk.js"
+  },
+  {
+    "revision": "836723d972dd52042980",
+    "url": "/BlogsApp/static/js/runtime~main.dca05309.js"
+  },
+  {
+    "revision": "e31244c8a0786e8c62b9b9e832f49db2",
+    "url": "/BlogsApp/static/media/info.e31244c8.svg"
+  },
+  {
+    "revision": "004ab6a28a0dbf883fc5e0421a064121",
+    "url": "/BlogsApp/static/media/mainPageImg.004ab6a2.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BlogsApp/precache-manifest.d76f3f88fc46f53d2e94456eb0cd1af1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BlogsApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
